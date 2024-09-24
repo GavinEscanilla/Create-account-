9add979eaee274144060d1f3283261070847e427
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-09-24 19:01:25 -0400

    added functionailty and animation for  name and submit
    if chars are not between 6-18 display alert, if between 6-18 display green checkmark with fade in animation

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
- $(".btn").addClass('animate__animated animate__bounce');
+ $("#form").addClass('animate__animated animate__bounce');
 
+
+
+ $('#submit').click(function(){
+    const alert = "<div class=\'alert alert-danger\' role=\"alert\">Type name between 6-18 characters</div>";
+    const check = '<i class="fa-solid fa-check"></i>';
+ if($("#name").val().length < 6 || $('#name').val().length > 17 ){
+    if($('.alert').length === 0){
+ $('#form').append(alert);
+    }
+ $('.fa-check').remove();
+    
+ }else{
+    $(".alert").remove();
+    if($('.fa-check').length === 0){
+    $('#name-container').append(check);
+    $('.fa-check').addClass('animate__animated animate__fadeIn ')
+ }
+}
+});
 });
