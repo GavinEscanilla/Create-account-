03e519b5cfc9f43591c6e18d9d7f72ed9c7bb4f1
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-09-25 16:39:25 -0400

    fixed the alert bug with email and name

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ $(document).ready(function(){
     const alert2 = "<div class=\'alert alert-danger\' id=\"alert2\" role=\"alert\">Type in a valid email</div>";
     const check = '<i class="fa-solid fa-check"></i>';
  if($("#name").val().length < 6 || $('#name').val().length > 17 ){
-    if($('#alert1','#name-container').length === 0){
+    if($('#name-container > #alert1').length === 0){
  $('#name-container').append(alert1);
     }
- $('.fa-check', '#name-container').remove();
+ $('#name-container > .fa-check').remove();
     $("#name-container").addClass('animate__animated animate__shakeX');
  }else{
-    $("#alert1",'#name-container').remove();
-    if($('.fa-check').length === 0){
-    $('#name-container').append(check);
-    $('.fa-check').addClass('animate__animated animate__fadeIn ');
+    $("#name-container > #alert1").remove();
+    if($('#name-container > .fa-check').length === 0){
+       $('#name-container').append(check);
+       $('#name-container > .fa-check').addClass('animate__animated animate__fadeIn ');
  }
 }
 if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($("#email").val())){
-   $("#alert2",'#email-container').remove();
-   if($('.fa-check','#email-container').length === 0){
-   $('#email-container').append(check);
-   $('.fa-check').addClass('animate__animated animate__fadeIn ')
+   $('#email-container > #alert2').remove();
+   if($('#email-container > .fa-check').length === 0){
+    $('#email-container').append(check);
+    $('#email-container > .fa-check').addClass('animate__animated animate__fadeIn ')
+   }
 }else{
-   if($("#alert2",'#email-container').length === 0){
+   if($('#email-container > #alert2').length === 0){
       $('#email-container').append(alert2)
    }
-   $(".fa-check","#email-container").remove();
-   $("#email-container").addClass('animate__animated animate__fadeIn ');
-}
-}
-});
-});
+   $("#email-container > .fa-check").remove();
+   $("#email-container").addClass('animate__animated animate__shakeX');
+         }
+      });
+   });
 
