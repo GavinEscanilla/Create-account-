d48a166a3556bcaa92e7349159625967a8f8283c
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-09-25 01:31:59 -0400

    added email but having trouble with detecting a inavlid email

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 $(document).ready(function(){
  $("#form").addClass('animate__animated animate__bounce');
-
-
-
  $('#submit').click(function(){
-    const alert = "<div class=\'alert alert-danger\' role=\"alert\">Type name between 6-18 characters</div>";
+    const alert1 = "<div class=\'alert alert-danger\' id=\"alert1\" role=\"alert\">Type name between 6-18 characters</div>";
+    const alert2 = "<div class=\'alert alert-danger\' id=\"alert2\" role=\"alert\">Type in a valid email</div>";
     const check = '<i class="fa-solid fa-check"></i>';
  if($("#name").val().length < 6 || $('#name').val().length > 17 ){
-    if($('.alert').length === 0){
- $('#form').append(alert);
+    if($('#alert1','#name-container').length === 0){
+ $('#name-container').append(alert1);
     }
- $('.fa-check').remove();
-    
+ $('.fa-check', '#name-container').remove();
+    $("#name-container").addClass('animate__animated animate__shakeX');
  }else{
-    $(".alert").remove();
+    $("#alert1",'#name-container').remove();
     if($('.fa-check').length === 0){
     $('#name-container').append(check);
-    $('.fa-check').addClass('animate__animated animate__fadeIn ')
+    $('.fa-check').addClass('animate__animated animate__fadeIn ');
  }
+}
+if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($("#email").val())){
+   $("#alert2",'#email-container').remove();
+   if($('.fa-check','#email-container').length === 0){
+   $('#email-container').append(check);
+   $('.fa-check').addClass('animate__animated animate__fadeIn ')
+}else{
+   if($("#alert2",'#email-container').length === 0){
+      $('#email-container').append(alert2)
+   }
+   $(".fa-check","#email-container").remove();
+   $("#email-container").addClass('animate__animated animate__fadeIn ');
+}
 }
 });
 });
+
